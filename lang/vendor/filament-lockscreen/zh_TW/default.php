dccbce5ca366d751c39268c4de78ee4d06991c55
Henry Lee <henryleeworld@gmail.com>
2024-01-26 00:04:42 +0800

    feat:初始化

---
+++ lang/vendor/filament-lockscreen/zh_TW/default.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'heading' => '螢幕鎖定',
+    'user_menu_title' => '螢幕鎖定',
+    'fields' => [
+        'password' => '密碼',
+    ],
+    'button' => [
+        'switch_account' => '選擇帳戶',
+        'submit_label' => '登入',
+    ],
+    'notification' => [
+        'title' => '登入失敗',
+        'message' => '登入失敗後，您已被重新導向至登入頁面。',
+    ],
+];
